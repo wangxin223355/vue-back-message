7addaa1a4e693e4650e0e8fe27be24c8ee5c3009
重科前端小鑫 <2677855779@qq.com>
2020-04-27 23:24:09 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+// 导入全局样式表
+import './assets/css/global.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+
+// 导入axios
+import axios from 'axios'
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 每一个vue组件都可以通过访问$http使用ajax请求
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+
+// 将Message挂载到Vue全局 每个组件都可以访问
+Vue.prototype.$message = Message
