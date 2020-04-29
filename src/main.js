74dba4ba86324a7043badc42fdf0b88cb3edcb7f
重科前端小鑫 <2677855779@qq.com>
2020-04-29 10:19:30 +0800

    完成用户列表的开发

--- src/main.js
+++ src/main.js
@@ -11,6 +11,13 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 设置请求拦截器
+axios.interceptors.request.use(config => {
+  // 为请求头对象添加 token 验证的 Authorization 对象
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须return
+  return config
+})
 // 每一个vue组件都可以通过访问$http使用ajax请求
 Vue.prototype.$http = axios
 
