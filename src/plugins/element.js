808ff516c0aa0522f4212c689b0747e41c83b930
重科前端小鑫 <2677855779@qq.com>
2020-05-03 11:28:46 +0800

    完成订单功能的开发

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -37,7 +37,9 @@ import {
     Step,
     CheckboxGroup,
     Checkbox,
-    Upload
+    Upload,
+    Timeline,
+    TimelineItem
 } from 'element-ui'
 
 // 注册为全局组件
@@ -76,6 +78,8 @@ Vue.use(Steps)
 Vue.use(Checkbox)
 Vue.use(CheckboxGroup)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 // 将Message挂载到Vue全局 每个组件都可以访问
 Vue.prototype.$message = Message
